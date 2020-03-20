e59bf7d7ab8048b6afd922d9b6968c63e17e7a58
Matteo Magni <matteo@magni.me>
2020-03-20 22:14:06 +0100

    feat (Ansible) playbook for deploy laravel app

--- terraform/main.tf
+++ terraform/main.tf
@@ -90,6 +90,13 @@ resource "aws_security_group" "production" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["172.31.0.0/16"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
