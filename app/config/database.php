8fd83d6f11d73e2d6a08e8c592597dcc5ecaec4d
Matteo Magni <matteo@magni.me>
2020-04-01 13:23:09 +0000

    feat (Laravel): created user controller for demo

--- app/config/database.php
+++ app/config/database.php
@@ -66,8 +66,20 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
+            // 'host' => env('DB_HOST', '127.0.0.1'),
+	    'read' => [
+	        'host' => [
+	            env('DB_HOST', '127.0.0.1')
+	        ],
+		'port' => env('DB_PORT_READ', '5432')
+	    ],
+	    'write' => [
+	        'host' => [
+	            env('DB_HOST', '127.0.0.1')
+	         ],
+		'port' => env('DB_PORT_WRITE', '5432')
+	    ],
+            // 'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
